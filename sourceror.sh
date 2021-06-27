9864e868d731f3fb783bfbc25f368a3e13240a1f
deeperlearner <b04202035@g.ntu.edu.tw>
2021-06-27 22:45:44 +0800

    fix bug

--- sourceror.sh
+++ sourceror.sh
@@ -2,18 +2,18 @@
 
 Append () {
     if [ -e "$HOME/$FILE" ]; then
-        if grep -q "^$1 ~/${FILE}.d/*" "$HOME/$FILE"; then
-            echo "$1 has already been added."
+        if grep -q "$1" "$HOME/$FILE"; then
+            echo "Append done."
         else
-            echo -en "\n$1 ~/${FILE}.d/*" >> "$HOME/$FILE"
+            echo -en "\n$1" >> "$HOME/$FILE"
         fi
     else
         echo "File $HOME/$FILE does not exist. Create a new one."
-        echo -en "\n$1 ~/${FILE}.d/*" >> "$HOME/$FILE"
+        echo -en "\n$1" >> "$HOME/$FILE"
     fi
 }
 
 for FILE in .bashrc
 do
-    Append "source"
+    Append "source ~/${FILE}.d/${FILE}_local"
 done
